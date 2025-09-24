ef67e3e59a83328b7d9b01c883e05099669134fb
MShashikant <shashikantmishra045@gmail.com>
2025-09-24 17:29:00 +0000

    container config done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountID = 772690
+let accountEmail = "shashikant@m.com"
+var accountPassword = "12345"
+accountCity = "Ayodhya"
+
+// accountID = 3  // not allowed
+
+accountEmail = "shashikant@mishra.com"
+accountPassword = "123"
+accountCity = "Gonda"
+
+console.log(accountID);
+console.table([accountID, accountEmail, accountPassword, accountCity])
+
